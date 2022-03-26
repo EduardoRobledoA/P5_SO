22e105009b5d2b8c7d1a3f1c26f50f448b255b17
EduardoRobledoA <robledoaguirre@hotmail.com>
2022-03-26 13:37:19 -0600

    V1.0
    Creación Proyecto y prueba repositorio

--- src/simuladorprocesos2/SimuladorProcesos2.java
+++ src/simuladorprocesos2/SimuladorProcesos2.java
@@ -17,6 +17,7 @@ public class SimuladorProcesos2 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("HOLA MUNDO");
+        System.out.println("Prueba repositorio");
     }
     
 }
